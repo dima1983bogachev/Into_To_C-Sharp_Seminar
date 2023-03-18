0fdd5b69edb120ad872b232a0587ffa076793705
dima1983bogachev <dima1983bogachev@yandex.ru>
2023-03-18 05:38:05 +0300

    Программа поиска наибольшего из двух

--- Seminar_01/Example_02/Program.cs
+++ Seminar_01/Example_02/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Введите число a");
 int NumberA = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите число b");
 int NumberB = Convert.ToInt32(Console.ReadLine());
 
