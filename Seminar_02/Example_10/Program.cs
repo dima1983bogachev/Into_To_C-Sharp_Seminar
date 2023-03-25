6442a109ded546f9c05d22b1c5d4c919ee41c55f
dima1983bogachev <dima1983bogachev@yandex.ru>
2023-03-26 00:34:12 +0300

    Add Example_13

--- Seminar_02/Example_10/Program.cs
+++ Seminar_02/Example_10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа*/
+
+
+Console.WriteLine("Введите трехзначное число:");
+
+string NumberString = Console.ReadLine();
+
+char[] Number = NumberString.ToCharArray();
+
+Console.WriteLine($"Вторая цифра числа: {Number[1]}");
+
+
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
---
+++ Seminar_02/Example_13/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+
+string NumberString = Console.ReadLine();
+char[] Number = NumberString.ToCharArray();
+
+int Index = Number.Length;
+if (Index <= 2)
+{
+    Console.WriteLine("Третьей цифры числа нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа: {Number[2]}");
+}
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+/*
+int IndexOf(char[] number, int find)
+{
+    int index = number.Length;
+    int count = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    Return position;
+}
+
+int ThreeIndex = IndexOf(Number, 2);
+*/
+
+
