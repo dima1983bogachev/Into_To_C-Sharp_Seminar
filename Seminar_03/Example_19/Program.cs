35f7e522a0bf0b91d84f29019679a28fd4d36d04
dima1983bogachev <dima1983bogachev@yandex.ru>
2023-03-26 02:54:06 +0300

    ЗАдача 19, решение через превращения числа в маасив, его реверс и сравнение. Проблема со сравнением массивов

--- Seminar_03/Example_19/Program.cs
+++ Seminar_03/Example_19/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 19
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+Console.WriteLine("Введите 5-ти значное число");
+string NumberString = Console.ReadLine();
+// Перевод числа из строки в массив символов
+char[] Number = NumberString.ToCharArray();
+// метод вывода массива 
+void PrintArray(char[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+//Метода реверса массива
+void ReverseArray(char[] array)
+{
+    for (int i = 0; i < array.Length - i - 1; i++)
+    {
+        char temporary = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temporary;
+    }
+}
+
+// PrintArray(Number);
+
+char[] reversedNumber = Number;
+ReverseArray(reversedNumber);
+PrintArray(reversedNumber);
+
+
+if (reversedNumber == Number) Console.WriteLine("Да, это палиндром");
+else Console.WriteLine("Нет, это не палиндром");
+
