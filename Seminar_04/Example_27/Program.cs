21207efaec1c311f4c68be3f7dcd5a805811be05
dima1983bogachev <dima1983bogachev@yandex.ru>
2023-03-26 06:31:48 +0300

    Решена задача №27

---
+++ Seminar_04/Example_27/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Перевод числа из строки в тип int
+Console.WriteLine("Введите число");
+string NumberString = Console.ReadLine();
+int Number = Convert.ToInt32(NumberString);
+
+// Строка преобразуется в массив символов, чтоб определить количество разрядов числа по максимальной длине массива
+char[] NumberIndex = NumberString.ToCharArray();
+
+int result = 0;
+
+for (int i = NumberIndex.Length; i >= 0; i--)
+{
+    result += Number / (int)Math.Pow(10, i);
+    Number = Number % (int)Math.Pow(10, i);
+}
+Console.WriteLine(result);
+
